5f55e18ce0dc10edef226bfc271ce89654081d1f
CAM TU <trantcamtu05@gmail.com>
2025-12-18 10:51:13 +0700

    Cập nhật config.example.php và .gitignore

---
+++ config.example.php
@@ -0,0 +1,7 @@
+<?php
+// Thông tin cấu hình cơ sở dữ liệu
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'tracnghiem');
+define('DB_USER', 'root');
+define('DB_PASS', ''); // đặt password MySQL của bạn
+?>
